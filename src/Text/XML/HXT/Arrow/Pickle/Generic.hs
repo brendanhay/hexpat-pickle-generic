fe3be006b36d7c0d05792ffeec21f4acb77c6317
Brendan Hay <brendan.g.hay@gmail.com>
2013-08-12 13:47:35 +0200

    Applying configurable options for record field labels and constructor tags

--- src/Text/XML/HXT/Arrow/Pickle/Generic.hs
+++ src/Text/XML/HXT/Arrow/Pickle/Generic.hs
@@ -1,10 +1,14 @@
-{-# LANGUAGE DefaultSignatures   #-}
-{-# LANGUAGE FlexibleContexts    #-}
-{-# LANGUAGE FlexibleInstances   #-}
-{-# LANGUAGE IncoherentInstances #-}
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeOperators       #-}
+{-# LANGUAGE DefaultSignatures               #-}
+{-# LANGUAGE FlexibleContexts                #-}
+{-# LANGUAGE FlexibleInstances               #-}
+{-# LANGUAGE OverloadedStrings               #-}
+{-# LANGUAGE RecordWildCards                 #-}
+{-# LANGUAGE ScopedTypeVariables             #-}
+{-# LANGUAGE TypeOperators                   #-}
+{-# LANGUAGE ViewPatterns                    #-}
+
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+{-# OPTIONS_GHC -fno-warn-orphans            #-}
 
 -- |
 -- Module      : Text.XML.HXT.Arrow.Pickle.Generic
@@ -19,13 +23,29 @@
 
 module Text.XML.HXT.Arrow.Pickle.Generic where
 
-import Data.Char                        (toLower)
+import Data.Char                        (isLower, toLower)
 import GHC.Generics
 import Text.XML.HXT.Arrow.Pickle
 import Text.XML.HXT.Arrow.Pickle.Schema
 import Text.XML.HXT.Arrow.Pickle.Xml
 
+--
+-- Options
+--
+
 data Options = Options
+    { constructorTagModifier :: String -> String
+      -- ^ Function applied to constructor tags.
+    , fieldLabelModifier     :: String -> String
+      -- ^ Function applied to record field labels.
+    }
+
+defaultOptions :: Options
+defaultOptions = Options id (dropWhile isLower)
+
+--
+-- IsXML
+--
 
 class IsXML a where
     pickleXML :: Options -> PU a
@@ -33,60 +53,74 @@ class IsXML a where
     default pickleXML :: (Generic a, GIsXML (Rep a)) => PU a
     pickleXML = genericPickleXML defaultOptions
 
-genericPickleXML :: (Generic a, GIsXML (Rep a)) => Options -> PU a
+class GIsXML f where
+    gPickleXML :: Options -> PU a -> PU (f a)
+
 genericPickleXML opts =
     (to, from) `xpWrap` (gPickleXML opts) (genericPickleXML opts)
 
-defaultOptions :: Options
-defaultOptions = Options
-
-class GIsXML f where
-    gPickleXML :: Options -> PU p -> PU (f p)
+--
+-- Generic Instances
+--
 
 instance GIsXML a => GIsXML (M1 i c a) where
-    -- Meta-information, which is not handled elsewhere
     gPickleXML opts = xpWrap (M1, unM1) . gPickleXML opts
 
 instance XmlPickler a => GIsXML (K1 i a) where
-    -- Constant values are decoded using their XmlPickler instance
     gPickleXML _ _ = (K1, unK1) `xpWrap` xpickle
 
 instance GIsXML U1 where
-    -- Empty constructors
     gPickleXML _ _ = (const U1, const ()) `xpWrap` xpUnit
 
 instance (GIsXML f, GIsXML g) => GIsXML (f :+: g) where
-    -- Sums: Encode choice between constructors
     gPickleXML opts f = gPickleXML opts f `xpSum` gPickleXML opts f
 
 instance (GIsXML f, GIsXML g) => GIsXML (f :*: g) where
-    -- Products: Encode multiple argument constructors
     gPickleXML opts f = xpWrap
         (uncurry (:*:), \(a :*: b) -> (a, b))
         (gPickleXML opts f `xpPair` gPickleXML opts f)
 
 instance (Constructor c, GIsXML f) => GIsXML (M1 C c f) where
-    gPickleXML opts f = xpElem
-        (map toLower $ conName (undefined :: M1 C c f r))
+    gPickleXML opts@Options{..} f = xpElem
+        (constructorTagModifier $ conName (undefined :: M1 C c f r))
         ((M1, unM1) `xpWrap` (gPickleXML opts f))
 
 instance (Selector s, GIsXML f) => GIsXML (M1 S s f) where
-    gPickleXML opts f = xpElem
-        (map toLower $ selName (undefined :: M1 S s f r))
+    gPickleXML opts@Options{..} f = xpElem
+        (fieldLabelModifier $ selName (undefined :: M1 S s f r))
         ((M1, unM1) `xpWrap` gPickleXML opts f)
 
 --
--- Pickler Combinators
+-- Concrete Instances
+--
+
+instance XmlPickler Bool where
+    xpickle = (inp, out) `xpWrap` xpText
+      where
+        inp (lower -> "true")  = True
+        inp (lower -> "false") = False
+        inp _ = error "No parse for bool in toBool (XmlPickler)."
+
+        out True  = "true"
+        out False = "false"
+
+        lower = map toLower
+
+instance GIsXML (K1 i String) where
+    gPickleXML _ _ = (K1, unK1) `xpWrap` xpText0
+
+--
+-- Combinators
 --
 
 xpSum :: PU (f r) -> PU (g r) -> PU ((f :+: g) r)
-xpSum l r = (i, o) `xpWrap` xpEither l r
+xpSum left right = (inp, out) `xpWrap` xpEither left right
   where
-    i (Left  x) = L1 x
-    i (Right x) = R1 x
+    inp (Left  x) = L1 x
+    inp (Right x) = R1 x
 
-    o (L1 x) = Left x
-    o (R1 x) = Right x
+    out (L1 x) = Left x
+    out (R1 x) = Right x
 
 xpEither :: PU a -> PU b -> PU (Either a b)
 xpEither ~(PU fl tl sa) ~(PU fr tr sb) = PU pickle unpickle schema
@@ -96,26 +130,9 @@ xpEither ~(PU fl tl sa) ~(PU fr tr sb) = PU pickle unpickle schema
 
     unpickle = UP $ \x ->
         case runUP tl x of
-            (Left err, _) -> lmap (fmap Right) (runUP tr x)
-            r             -> lmap (fmap Left) r
+            (Left _, _) -> lmap (fmap Right) (runUP tr x)
+            r           -> lmap (fmap Left) r
 
     lmap f (a, b) = (f a, b)
 
     schema = sa `scAlt` sb
-
---
--- Instances
---
-
-instance XmlPickler Bool where
-    xpickle = (toBool, fromBool) `xpWrap` xpText
-      where
-        toBool "true"  = True
-        toBool "false" = False
-        toBool _       = error "No parse for bool in toBool (XmlPickler)."
-
-        fromBool True  = "true"
-        fromBool False = "false"
-
-instance GIsXML (K1 i String) where
-    gPickleXML _ _ = (K1, unK1) `xpWrap` xpText0
